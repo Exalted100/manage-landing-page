006113f7a8268c4eff99d3ebf3e1c38e16881149
Fawaz Haroun <harounayomikun@gmail.com>
2021-07-11 22:11:30 +0100

    responsiveness

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const submitButton = document.querySelector(".submit-button")
+const hamburgerIcon = document.querySelector(".hamburger-icon")
+const mobileNavMenu = document.querySelector(".nav-bar-mobile-container")
+const mobileNavContainer = document.querySelector(".mobile-nav")
 
-const checkMail = (event) => {
+const checkMail = () => {
     try {
         const address = document.querySelector(".email-input").value
         address = new MailAddress(address).Address;
@@ -9,4 +12,25 @@ const checkMail = (event) => {
     }
 }
 
+let openHamburger = true
+
+const toggleHamburger = () => {
+    if (openHamburger) {
+        hamburgerIcon.setAttribute("src", "./images/icon-close.svg")
+        mobileNavMenu.style.display = "block"
+        mobileNavContainer.style.backgroundColor = "rgba(250, 250, 250, 0.3)"
+        mobileNavContainer.style.width = "100%"
+        mobileNavContainer.style.height = "100%"
+        mobileNavContainer.style.top = "0"
+        mobileNavContainer.style.position = "fixed"
+        openHamburger = false
+    } else {
+        hamburgerIcon.setAttribute("src", "./images/icon-hamburger.svg")
+        mobileNavMenu.style.display = "none"
+        mobileNavContainer.style.backgroundColor = "none"
+        openHamburger = true
+    }
+}
+
+hamburgerIcon.addEventListener("click", toggleHamburger)
 submitButton.addEventListener("click", checkMail)
